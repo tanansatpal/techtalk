b5e25d5daeee1cdab3906083d089c6c26a35e8e4
Satpal Tanan <tanansatpal@gmail.com>
2021-02-02 15:48:53 +0530

    Memory leak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-sub *ngIf="hide"></app-sub>`
 })
 export class AppComponent {
-  title = 'techtalk';
+  hide = false;
+
+  constructor() {
+    setInterval(() => this.hide = !this.hide, 50);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SubComponent } from './sub.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +15,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
